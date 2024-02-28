573ddcfa085a93fc9e05e8f8eb65cc9ef28dc711
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-02-28 12:13:41 -0500

    Push updated mlePossum() with documentation and helpers

--- R/mlePossum_helpers.R
+++ R/mlePossum_helpers.R
@@ -1,99 +1,61 @@
-#score function (DOES NOT HANDLE NULL Z APPROPRIATELY)
-U = function(beta, X, Y, Z = NULL, data) {
+loglik_mat = function(beta_eta, 
+                      Y_name, X_name, 
+                      Z_name = NULL, Xstar_name, 
+                      Q_name, data,
+                      verbose = FALSE) {
+  #print(beta_eta)
   
-  #turn inputs into matrices
-  beta = matrix(data = beta, ncol = 1)
-  data = data.matrix(frame = data) #holds X, Z, and other potential covariates
+  # Save useful constants
+  N = nrow(data) ## Phase I sample size
+  n = sum(data[, Q_name]) ## Phase II sample size
   
-  #initialize lambda, score
-  if(is.null(Z)){
-    l = exp(beta[1] + beta[2] * data[, X]) 
-  }
-  else {
-    l = exp(beta[1] + data[, c(X, Z)] %*% beta[-1]) ## dimension n x 1
-  }
-  score = matrix(data = 0, 
-                  nrow = nrow(beta)) ## dimension p x 1 (where p = dim(beta))
+  # Reorder data to put queried rows first
+  data = data[order(data[, Q_name], decreasing = TRUE), ]
   
-  # save Y - lambda 
-  yml = data[, Y] - l ## dimension n x 1
-  
-  #fill first element of score
-  score[1] = sum(yml)
-  
-  #save column copies of Y - lambda in a matrix
-  ## same column, replicated once for each covariate in (X, Z)
-  yml_wide = matrix(data = yml, 
-                     nrow = length(yml),
-                     ncol = length(c(X, Z)), 
-                     byrow = FALSE) ## dimension n x (p - 1)
-  
-  #fill remaining elements of score (clever way)
-  score[-1] = colSums(data[, c(X, Z)] * yml_wide)
-  
-  #return score
-  score
-}
-
-#Fisher information
-info = function(beta, X, Z = NULL, data) {
-  #turn inputs into matrices
-  beta = matrix(data = beta, ncol = 1)
-  data = data.matrix(frame = data) #holds X, Z, and other potential covariates
-  
-  #initialize lambda
-  if(is.null(Z)) {
-    l = exp(beta[1] + beta[2] * data[, X])  
+  # Create matrix of complete data
+  if (n < N) {
+    queried_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
+    unqueried_data = rbind(
+      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 0, data[-c(1:n), c(Z_name, Xstar_name)]),
+      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 1, data[-c(1:n), c(Z_name, Xstar_name)])
+    )
+    colnames(unqueried_data) = c("id", Y_name, X_name, Z_name, Xstar_name)
+    complete_data = data.matrix(rbind(queried_data, unqueried_data))
   } else {
-    l = exp(beta[1] + data[, c(X,Z)] %*% beta[-1]) #dim n x 1
+    complete_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
   }
   
-  #save top left 2x2
-  mini_top_left = sum(l)
-  mini_top_right = sum(data[, X] * l)
-  mini_bottom_left = mini_top_right
-  mini_bottom_right = sum(data[,X]^2 * l)
+  # Compute log-likelihood 
+  ## P(Y|X,Z) from Poisson distribution
+  lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name] + beta_eta[3] * complete_data[, Z_name])
   
-  #create empty information matrix 
-  i = matrix(data = 0, 
-              nrow = nrow(beta), 
-              ncol = nrow(beta))
-  i[1, 1] = mini_top_left
-  i[1, 2] = i[2, 1] = mini_bottom_left
-  i[2, 2] = mini_bottom_right 
+  ### Dazzle fix: replace y with data[, Y_name]
+  pYgivXZ = dpois(x = complete_data[, Y_name], lambda = lambdaY)
   
-  if (!is.null(Z)) {
-    #extend bottom left vector (sum of product of Z and lambda)
-    l_wide = matrix(data = l, 
-                     nrow = length(l),
-                     ncol = length(Z), 
-                     byrow = FALSE) 
-    bottom_left = colSums(data[, Z] * l_wide)
-    i[-c(1:2), 1] = bottom_left
-    i[1, -c(1:2)] = t(bottom_left)
-    
-    #extend bottom middle vector (sum of product of X, Z, and lambda)
-    xl = data[,X] * l
-    xl_wide = matrix(data = xl,
-                      nrow = length(xl),
-                      ncol = length(Z),
-                      byrow = FALSE)
-    bottom_middle = colSums(data[, Z] * xl_wide)
-    i[-c(1:2), 2] = bottom_middle
-    i[2, -c(1:2)] = t(bottom_middle)
-    
-    #extend bottom right matrix
-    #zzt = data[, Z] %*% t(data[, Z]) #issue in dimensionality
-    zz = data[,Z] * data[,Z] #temp issue fix
-    
-    #l_wide = matrix(data = l, 
-    #/nrow = length(l),
-    #ncol = ncol(zz),
-    #ncol = ncol(zzt), #same issue
-    #byrow = FALSE) 
-    #bottom_right = colSums(zzt * l_wide) #issue in dimensionality
-    bottom_right = sum(zz * l) #temp issue fix
-    i[-c(1:2), -c(1:2)] = bottom_right
-  }
-  return(-i)
+  ## P(X|X*,Z) from Bernoulli distribution
+  pXgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name]))) ^ complete_data[, X_name] * 
+    (1 - 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name])))) ^ (1 - complete_data[, X_name]) 
+  
+  ## P(Y, X|X*, Z) 
+  pYXgivXstarZ = pYgivXZ * pXgivXstarZ
+  
+  ## Marginalize X out of P(Y, X|X*, Z) for unqueried 
+  marg_pYXgivXstarZ = rowsum(x = pYXgivXstarZ, 
+                             group = complete_data[, "id"])
+  
+  ### Dazzle fix: replace with another VERY small number that's close to 0
+  pYgivXZ[which(pYgivXZ == 0)] = 5e-324
+  pXgivXstarZ[which(pXgivXstarZ == 0)] = 5e-324
+  marg_pYXgivXstarZ[which(marg_pYXgivXstarZ == 0)] = 5e-324
+  
+  # Compute log-likelihood
+  ll = sum(log(pYgivXZ[c(1:n)])) + 
+    sum(log(pXgivXstarZ[c(1:n)])) + 
+    sum(log(marg_pYXgivXstarZ[-c(1:n)]))
+  if(verbose) {print(paste("Queried:", ll))}
+  
+  ll = ll + 
+    sum(log(marg_pYXgivXstarZ[-c(1:n)]))
+  if(verbose) {print(paste("Queried + Unqueried:", ll))}
+  return(-ll) ## return (-1) x log-likelihood for maximization
 }
