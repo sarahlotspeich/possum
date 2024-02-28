f392ed1d4eb6f845a13e3e23f4ce03ed41ac3d17
Ashley Mullan <ashleymullan2000@gmail.com>
2024-02-28 15:54:02 -0500

    push updated mlePossum_helpers
    option to ignore Z

--- R/mlePossum_helpers.R
+++ R/mlePossum_helpers.R
@@ -13,30 +13,55 @@ loglik_mat = function(beta_eta,
   data = data[order(data[, Q_name], decreasing = TRUE), ]
   
   # Create matrix of complete data
-  if (n < N) {
-    queried_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
+  if(!is.null(Z_name)){ #case with covariates
+    if (n < N) {
+      queried_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
+      unqueried_data = rbind(
+        cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 0, data[-c(1:n), c(Z_name, Xstar_name)]),
+        cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 1, data[-c(1:n), c(Z_name, Xstar_name)])
+      )
+      colnames(unqueried_data) = c("id", Y_name, X_name, Z_name, Xstar_name)
+      complete_data = data.matrix(rbind(queried_data, unqueried_data))
+    } else {
+      complete_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
+    }
+  }
+  else{ #case without covariates
+    queried_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Xstar_name)])
     unqueried_data = rbind(
-      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 0, data[-c(1:n), c(Z_name, Xstar_name)]),
-      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 1, data[-c(1:n), c(Z_name, Xstar_name)])
+      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 0, data[-c(1:n), c(Xstar_name)]),
+      cbind(id = (n+1):N, data[-c(1:n), Y_name], X_name = 1, data[-c(1:n), c(Xstar_name)])
     )
-    colnames(unqueried_data) = c("id", Y_name, X_name, Z_name, Xstar_name)
+    colnames(unqueried_data) = c("id", Y_name, X_name, Xstar_name)
     complete_data = data.matrix(rbind(queried_data, unqueried_data))
   } else {
-    complete_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Z_name, Xstar_name)])
+    complete_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Xstar_name)])
+  }
   }
   
   # Compute log-likelihood 
   ## P(Y|X,Z) from Poisson distribution
-  lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name] + beta_eta[3] * complete_data[, Z_name])
-  
+  if(!is.null(Z_name)){
+    lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name] + beta_eta[3] * complete_data[, Z_name])
+  }
+  ## P(Y|X) from Poisson distribution
+  else{
+    lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name])
+  }
   ### Dazzle fix: replace y with data[, Y_name]
   pYgivXZ = dpois(x = complete_data[, Y_name], lambda = lambdaY)
   
   ## P(X|X*,Z) from Bernoulli distribution
-  pXgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name]))) ^ complete_data[, X_name] * 
+  if(!is.null(Z_name)){
+    pXgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name]))) ^ complete_data[, X_name] * 
     (1 - 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name])))) ^ (1 - complete_data[, X_name]) 
-  
-  ## P(Y, X|X*, Z) 
+  }
+  else{
+  ## P(X|X*) from Bernoulli distribution
+  pXgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name]))) ^ complete_data[, X_name] * 
+    (1 - 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name])))) ^ (1 - complete_data[, X_name]) 
+  }
+  ## P(Y, X|X*, Z) OR P(Y,X|X*) 
   pYXgivXstarZ = pYgivXZ * pXgivXstarZ
   
   ## Marginalize X out of P(Y, X|X*, Z) for unqueried 
