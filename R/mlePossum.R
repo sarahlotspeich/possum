a62958e77714fc91ea39ffafbaf6e2ff2b6f8c7c
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-04-24 13:44:03 -0400

    Push updated attempt at no FNs

--- R/mlePossum.R
+++ R/mlePossum.R
@@ -1,57 +1,57 @@
 #' Maximum likelihood estimation for Poisson regression problems with covariate misclassification
 #' This function returns the maximum likelihood estimates (MLEs) for the Poisson regression model with covariate misclassification from Mullan et al. (2024+)
 #'
-#' @param error_formula misclassification model formula (or coercible to formula), a formula expression as for other regression models. The response should be the error-free version of the error-prone of the covariate. 
+#' @param error_formula misclassification model formula (or coercible to formula), a formula expression as for other regression models. The response should be the error-free version of the error-prone of the covariate.
 #' @param analysis_formula analysis model formula (or coercible to formula), a formula expression as for other regression models. The response should be the Poisson model outcome, and, if needed, the offset can be provided as the \code{offset} argument.
-#' @param offset optional, variable name string for the analysis model offset. Default is \code{offset = NULL} for no offset. 
+#' @param offset optional, variable name string for the analysis model offset. Default is \code{offset = NULL} for no offset.
 #' @param data dataset containing at least the variables included in \code{error_formula} and \code{analysis_formula}.
-#' @param noFN logical, if \code{noFN = FALSE} (the default), then it is assumed that there can be both false positives and false negatives in the error-prone exposure. If \code{noFN = TRUE}, the error mechanism is restricted to only false positives. 
+#' @param noFN logical, if \code{noFN = FALSE} (the default), then it is assumed that there can be both false positives and false negatives in the error-prone exposure. If \code{noFN = TRUE}, the error mechanism is restricted to only false positives.
 #' @return dataframe with final coefficient and standard error estimates for the analysis model
 #' @export
 
 mlePossum = function(error_formula, analysis_formula, offset = NULL, data, noFN = FALSE) {
-  ## Extract variable names from user-specified formulas 
+  ## Extract variable names from user-specified formulas
   get_Y_name = as.character(as.formula(analysis_formula))[2]
   get_X_name = as.character(as.formula(error_formula))[2]
-  
-  analysis_covar = unlist(strsplit(x = gsub(pattern = " ", 
-                                            replacement = "", 
-                                            x = as.character(as.formula(analysis_formula))[3]), 
-                                   split = "+", 
-                                   fixed = TRUE))
-  error_covar = unlist(strsplit(x = gsub(pattern = " ", 
-                                         replacement = "", 
-                                         x = as.character(as.formula(error_formula))[3]), 
-                                split = "+", 
-                                fixed = TRUE))
-  get_Xstar_name = setdiff(error_covar, analysis_covar) 
-  
-  get_Z_name = intersect(error_covar, analysis_covar) 
 
-  ## Fit complete-case models to get initial values 
-  ### P(Y|X,Z) 
+  analysis_covar = unlist(strsplit(x = gsub(pattern = " ",
+                                            replacement = "",
+                                            x = as.character(as.formula(analysis_formula))[3]),
+                                   split = "+",
+                                   fixed = TRUE))
+  error_covar = unlist(strsplit(x = gsub(pattern = " ",
+                                         replacement = "",
+                                         x = as.character(as.formula(error_formula))[3]),
+                                split = "+",
+                                fixed = TRUE))
+  get_Xstar_name = setdiff(error_covar, analysis_covar)
+
+  get_Z_name = intersect(error_covar, analysis_covar)
+
+  ## Fit complete-case models to get initial values
+  ### P(Y|X,Z)
   if (is.null(offset)) {
-    cc_fit = glm(formula = as.formula(analysis_formula), 
-                 data = data, 
+    cc_fit = glm(formula = as.formula(analysis_formula),
+                 data = data,
                  family = poisson)$coefficients
   } else {
-    cc_fit = glm(formula = as.formula(paste0(paste(get_Y_name, paste(get_X_name, collapse = "+"), sep = "~"), "+offset(log(", offset, "))")), 
-                 data = data, 
+    cc_fit = glm(formula = as.formula(paste0(paste(get_Y_name, paste(get_X_name, collapse = "+"), sep = "~"), "+offset(log(", offset, "))")),
+                 data = data,
                  family = poisson)$coefficients
   }
   ### P(X|X*,Z)
-  cc_fit = c(cc_fit, 
-             glm(formula = as.formula(error_formula), 
-                 data = data, 
+  cc_fit = c(cc_fit,
+             glm(formula = as.formula(error_formula),
+                 data = data,
                  family = binomial)$coefficients)
-  
+
   ## Add queried/non-missing data indicator
   data[, "Q"] = as.numeric(!is.na(data[, get_X_name]))
-  
+
   if (length(get_Z_name) > 0) {
-    optim_res = optim(fn = loglik_mat, 
-                      par = cc_fit, 
-                      hessian = TRUE, 
+    optim_res = optim(fn = loglik_mat,
+                      par = cc_fit,
+                      hessian = TRUE,
                       method = "BFGS",
                       Y_name = get_Y_name,
                       X_name = get_X_name,
@@ -62,9 +62,9 @@ mlePossum = function(error_formula, analysis_formula, offset = NULL, data, noFN 
                       data = data,
                       noFN = noFN)
   } else {
-    optim_res = optim(fn = loglik_mat, 
-                      par = cc_fit, 
-                      hessian = TRUE, 
+    optim_res = optim(fn = loglik_mat,
+                      par = cc_fit,
+                      hessian = TRUE,
                       method = "BFGS",
                       Y_name = get_Y_name,
                       X_name = get_X_name,
@@ -74,17 +74,17 @@ mlePossum = function(error_formula, analysis_formula, offset = NULL, data, noFN 
                       data = data,
                       noFN = noFN)
   }
-  
-  ## Prepare model output to be returned 
+
+  ## Prepare model output to be returned
   ### Invert the hessian to get estimated standard errors
   num_analysis_covar = length(analysis_covar) + 1
   cov_theta = tryCatch(expr = solve(optim_res$hessian)[1:num_analysis_covar, 1:num_analysis_covar],
                        error = function(err) {
-                         matrix(data = NA, 
-                                nrow = num_analysis_covar, 
+                         matrix(data = NA,
+                                nrow = num_analysis_covar,
                                 ncol = num_analysis_covar)
                        })
-  
+
   if (optim_res$convergence == 0) {
     res = data.frame(Est = optim_res$par[1:num_analysis_covar],
                      SE = sqrt(diag(cov_theta)))
@@ -94,8 +94,9 @@ mlePossum = function(error_formula, analysis_formula, offset = NULL, data, noFN 
                      SE = rep(NA, num_analysis_covar))
     rownames(res) = c("(Intercept)", analysis_covar)
   }
-  
+
   ## Return model output
-  return(list(coefficients = res, 
+  return(list(coefficients = res,
+              #misclass_coefficients = misclass_res,
               convergence = optim_res$convergence))
 }
--- R/mlePossum_helpers.R
+++ R/mlePossum_helpers.R
@@ -1,20 +1,18 @@
-loglik_mat = function(beta_eta, 
-                      Y_name, X_name, 
-                      Z_name = NULL, Xstar_name, 
-                      Q_name, 
+loglik_mat = function(beta_eta,
+                      Y_name, X_name,
+                      Z_name = NULL, Xstar_name,
+                      Q_name,
                       offset_name = NULL,
                       data,
                       noFN = FALSE,
                       verbose = FALSE) {
-  #print(beta_eta)
-  
   # Save useful constants
   N = nrow(data) ## Phase I sample size
   n = sum(data[, Q_name]) ## Phase II sample size
-  
+
   # Reorder data to put queried rows first
   data = data[order(data[, Q_name], decreasing = TRUE), ]
-  
+
   # Create matrix of complete data
   if(!is.null(Z_name)){ #case with covariates
     if (n < N) {
@@ -41,55 +39,67 @@ loglik_mat = function(beta_eta,
       complete_data = cbind(id = 1:n, data[1:n, c(Y_name, X_name, Xstar_name, offset_name)])
       }
   }
-  
-  # Compute log-likelihood 
+
+  # Compute log-likelihood
   if(!is.null(Z_name)){ ## P(Y|X,Z) from Poisson distribution
-    lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name] + beta_eta[3] * complete_data[, Z_name])  
+    lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name] + beta_eta[3] * complete_data[, Z_name])
     if(!is.null(offset_name)){ #has offset_name
       lambdaY = complete_data[, offset_name] * lambdaY
-    } 
+    }
   } else{ ## P(Y|X) from Poisson distribution
     lambdaY = exp(beta_eta[1] + beta_eta[2] * complete_data[, X_name])
     if(!is.null(offset_name)){ #has offset
       lambdaY = complete_data[, offset_name] * lambdaY
-    } 
+    }
   }
   ### Dazzle fix: replace y with data[, Y_name]
   pYgivXZ = dpois(x = complete_data[, Y_name], lambda = lambdaY)
-  
-  if(!is.null(Z_name)){ ## P(X|X*,Z) from Bernoulli distribution
-    expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name])))
-  } else{ ## P(X|X*) from Bernoulli distribution
-    expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[3] + beta_eta[4] * complete_data[, Xstar_name])))
-  }
-  
+
   ## P(X|X*,Z) from Bernoulli distribution
-  pXgivXstarZ = expit_XgivXstarZ ^ complete_data[, X_name] * (1 - expit_XgivXstarZ) ^ (1 - complete_data[, X_name])
+  ### or mixture if noFN = TRUE
   if (noFN) {
+    if(!is.null(Z_name)){ ## P(X|X*,Z) from Bernoulli distribution
+      expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Z_name])))
+    } else{ ## P(X|X*) from Bernoulli distribution
+      expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[3])))
+    }
+
+    ## P(X|X*,Z) from Bernoulli distribution
+    pXgivXstarZ = expit_XgivXstarZ ^ complete_data[, X_name] * (1 - expit_XgivXstarZ) ^ (1 - complete_data[, X_name])
+
     ## But if X* = 0, replace with point mass
     pXgivXstarZ[complete_data[, Xstar_name] == 0 & complete_data[, X_name] == 0] = 1 ### P(X=0|X*=0) = 1
     pXgivXstarZ[complete_data[, Xstar_name] == 0 & complete_data[, X_name] == 1] = 0 ### P(X=1|X*=0) = 0
-  } 
+  } else {
+    if(!is.null(Z_name)){ ## P(X|X*,Z) from Bernoulli distribution
+      expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[4] + beta_eta[5] * complete_data[, Xstar_name] + beta_eta[6] * complete_data[, Z_name])))
+    } else{ ## P(X|X*) from Bernoulli distribution
+      expit_XgivXstarZ = 1 / (1 + exp(-(beta_eta[3] + beta_eta[4] * complete_data[, Xstar_name])))
+    }
 
-  ## P(Y, X|X*, Z) OR P(Y,X|X*) 
+    ## P(X|X*,Z) from Bernoulli distribution
+    pXgivXstarZ = expit_XgivXstarZ ^ complete_data[, X_name] * (1 - expit_XgivXstarZ) ^ (1 - complete_data[, X_name])
+  }
+
+  ## P(Y, X|X*, Z) OR P(Y,X|X*)
   pYXgivXstarZ = pYgivXZ * pXgivXstarZ
-  
-  ## Marginalize X out of P(Y, X|X*, Z) for unqueried 
-  marg_pYXgivXstarZ = rowsum(x = pYXgivXstarZ, 
+
+  ## Marginalize X out of P(Y, X|X*, Z) for unqueried
+  marg_pYXgivXstarZ = rowsum(x = pYXgivXstarZ,
                              group = complete_data[, "id"])
-  
+
   ### Dazzle fix: replace with another VERY small number that's close to 0
   pYgivXZ[which(pYgivXZ == 0)] = 5e-324
   pXgivXstarZ[which(pXgivXstarZ == 0)] = 5e-324
   marg_pYXgivXstarZ[which(marg_pYXgivXstarZ == 0)] = 5e-324
-  
+
   # Compute log-likelihood
-  ll = sum(log(pYgivXZ[c(1:n)])) + 
-    sum(log(pXgivXstarZ[c(1:n)])) + 
+  ll = sum(log(pYgivXZ[c(1:n)])) +
+    sum(log(pXgivXstarZ[c(1:n)])) +
     sum(log(marg_pYXgivXstarZ[-c(1:n)]))
   if(verbose) {print(paste("Queried:", ll))}
-  
-  ll = ll + 
+
+  ll = ll +
     sum(log(marg_pYXgivXstarZ[-c(1:n)]))
   if(verbose) {print(paste("Queried + Unqueried:", ll))}
   return(-ll) ## return (-1) x log-likelihood for maximization
